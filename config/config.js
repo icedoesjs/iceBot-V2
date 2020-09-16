32100b032e6fec60724d3618fb97ea00056f3672
iceyym8 <40555230+iceyym8@users.noreply.github.com>
2020-09-16 09:06:00 -0400

    Create config.js

---
+++ config/config.js
@@ -0,0 +1,24 @@
+const config = {
+    "token": "TOKEN",
+    "prefix": "?",
+    "dbUrl": "MONGO-LINK",
+    "Developers": ["DEVELOPER-ID3"],
+    "colors": ["BLUE", "#A5F2F3"],
+    "footers": {
+        "default": "IceBot, Made By IceyyM8",
+        "credit": "IceBot",
+        "bot": "IceBot, The Discord Bot",
+        "write": "IceBot - Written with :heart:"
+    },
+    "supportDiscord": "https://discord.gg/8JHtRPm",
+    "Authors": ["IceyyM8"],
+    "activity": {
+        "text": "for ?",
+        "array": ["For {prefix}", "{members} users in {guilds} members", "Join our support server: {sServer}"],
+        "type": "WATCHING",
+        "changingAct": true
+    },
+    "apiKeys": ["YOTUBE_API_KEY", "SOUNDCLOUD_API_KEY", "AME_CLIENT_API_KEY]
+}
+
+module.exports = config;
